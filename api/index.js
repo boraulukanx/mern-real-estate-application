76d941b2f253b0dd0f1a22513df74077eff3b5f2
boraulukan <81370060+boraulukan@users.noreply.github.com>
2024-06-08 18:23:26 +0100

    created a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import { CommandSucceededEvent } from "mongodb";
 
 dotenv.config(); //used for initialise dotenv package
 
@@ -25,3 +26,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode: statusCode,
+    message,
+  });
+});
